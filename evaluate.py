c971616354995a41e9fa69111cf27dcae23fbf1c
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2019-09-29 20:35:33 +0300

    Add YOLOv3 and YOLOv3-tiny

--- evaluate.py
+++ evaluate.py
@@ -4,7 +4,7 @@ import numpy as np
 import argparse
 import sys
 import csv
-from importlib import import_module
+import time
 
 import cv2 as cv
 import common  # open_model_zoo
@@ -15,36 +15,33 @@ from pycocotools.cocoeval import COCOeval
 from datasets import *
 
 
-def evaluate(model, dataset, annotations, datasetName, classes):
-    assert(datasetName in ['COCO', 'VOC', 'COCO_80'])
-
-    topology = None
-    for entry in common.load_models(argparse.Namespace(config=None)):
-        if entry.name == model:
-            topology = entry
+def evaluate(model, dataset, annotations, datasetName, classes, useIR, perfTest):
+    net = None
+    # We do fake script location to nagivate to extra models from this repo
+    fake_loc = os.path.join(os.path.dirname(__file__), 'extra', 'models', 'yolo-v3', 'model.yml')
+    for path in [fake_loc, common.__file__]:
+        common.__file__ = path
+        for entry in common.load_models(argparse.Namespace(config=None)):
+            if entry.name == model:
+                # Dynamically import model class.
+                t = getattr(__import__('topologies'), model.replace('-', '_').replace('.', '_'))()
+                net = t.getOCVModel(useIR)  # Get OpenCV model instance (has postprocessing inside).
+                sys.modules.pop('topologies')
+                break
+        if net:
             break
 
-    if not topology:
+    if not net:
         print('Topology ' + model + ' not found!')
         sys.exit(1)
 
-    # Dynamically import model class.
-    t = getattr(__import__('topologies'), topology.name.replace('-', '_').replace('.', '_'))()
-    net = t.getOCVModel()  # Get OpenCV model instance (has postprocessing inside).
 
-    # Looks like a bug in OpenVINO:
-    # "Cannot parse parameter negative_slope from IR for ReLU."
-    # "Value 0,000000 cannot be casted to float"
     if net.getLayer(0).outputNameToIndex('image_info') != -1:
-        xmlPath, _ = t.getIR()
+        _, input_shapes, _  = net.getLayersShapes(netInputShape=None)
+        inpH, inpW = int(input_shapes[0][1][2]), int(input_shapes[0][1][3])
 
-        with open(xmlPath, 'rt') as f:
-            data = f.read()
-        data = data.replace('type="ReLU">', 'type="ReLU"><data negative_slope="0"/>')
-        with open(xmlPath, 'wt') as f:
-            f.write(data)
+        net.setInput(np.array([[inpH, inpW, 1.0]], dtype=np.float32), 'image_info')
 
-        net = t.getOCVModel()
 
     # NMS for YOLOv3
     nmsThreshold = 0.4 if len(net.getUnconnectedOutLayers()) > 1 else 0.0
@@ -53,6 +50,8 @@ def evaluate(model, dataset, annotations, datasetName, classes):
     detections = []
     images = os.listdir(dataset)
     for i, imgName in enumerate(images):
+        if perfTest and i == 10:
+            break
         print("%d/%d" % (i + 1, len(images)))
 
         img = cv.imread(os.path.join(dataset, imgName))
@@ -62,15 +61,13 @@ def evaluate(model, dataset, annotations, datasetName, classes):
 
         # TODO: fix Faster R-CNN with swapped inputs
         if net.getLayer(0).outputNameToIndex('image_info') != -1:
-            tensor_id = net.getLayer(0).outputNameToIndex('image_tensor')
-            assert(tensor_id != -1)
-
-            _, input_shapes, _  = net.getLayersShapes(netInputShape=None)
-            inpH, inpW = int(input_shapes[0][1][2]), int(input_shapes[0][1][3])
-
-            net.setInput(np.array([[inpH, inpW, 1.0]], dtype=np.float32), 'image_info')
             net.setInput(cv.dnn.blobFromImage(img, size=(inpW, inpH)), 'image_tensor')
+
+            start = time.time()
             out = net.forward().reshape(-1, 7)
+            if perfTest:
+                detections.append(time.time() - start)
+                continue
 
             classIds = []
             confidences = []
@@ -87,8 +84,18 @@ def evaluate(model, dataset, annotations, datasetName, classes):
                 h = int(detection[6] * img.shape[0]) - y + 1
                 boxes.append([x, y, w, h])
         else:
-            classIds, confidences, boxes = net.detect(img, confThreshold=confThreshold,
-                                                      nmsThreshold=nmsThreshold)
+            if perfTest:
+                if i == 0:
+                    net.predict(img)
+
+                # Run without postprocessing
+                start = time.time()
+                net.forward()
+                detections.append(time.time() - start)
+                continue
+            else:
+                classIds, confidences, boxes = net.detect(img, confThreshold=confThreshold,
+                                                          nmsThreshold=nmsThreshold)
 
         for classId, score, box in zip(classIds, confidences, boxes):
             classId = int(classId)
@@ -96,8 +103,10 @@ def evaluate(model, dataset, annotations, datasetName, classes):
                 categoryId = toCOCO(classId, VOC_classes)
             elif datasetName == 'COCO_80':
                 categoryId = toCOCO(classId, COCO_80_classes)
-            else:
+            elif datasetName == 'COCO':
                 categoryId = classId
+            else:
+                categoryId = toCOCO(classId, datasetName.split(','))
 
             detections.append({
               "image_id": int(imgName.rstrip('0')[:imgName.rfind('.')]),
@@ -113,6 +122,8 @@ def evaluate(model, dataset, annotations, datasetName, classes):
         # cv.imshow('detections', img)
         # cv.waitKey()
 
+    if perfTest:
+        return np.median(detections[1:])  # Exclude first run
 
     resFile = 'results.json'
     with open(resFile, 'wt') as f:
@@ -144,7 +155,7 @@ def evaluate(model, dataset, annotations, datasetName, classes):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Use this script to evaluate object detection networks on COCO 2017 validation dataset.')
-    parser.add_argument('-m', dest='model', required=True,
+    parser.add_argument('-m', dest='model',
                         help='Name of topology to evaluate')
     parser.add_argument('-i', dest='images', required=True,
                         help='Path to validation images folder. See http://cocodataset.org/#download')
@@ -154,25 +165,44 @@ if __name__ == '__main__':
                         help='Output file to write measured metrics')
     parser.add_argument('-c', dest='config', default='detection.yml',
                         help='Path to a config file')
+    parser.add_argument('-p', dest='perf', action='store_true',
+                        help='Run performance test')
     parser.add_argument('--classes', nargs='+', type=str, required=True,
                         help='A list of classes to be evaluated')
     args = parser.parse_args()
 
+    if args.output:
+        with open(args.output, 'wt') as f:
+            writer = csv.writer(f)
+            writer.writerow(['model'] + ['performance (ralative)'] if args.perf else args.classes)
+
     fs = cv.FileStorage(args.config, cv.FILE_STORAGE_READ)
     topologies = fs.getNode('topologies')
+    performance = {}
     for i in range(topologies.size()):
         entry = topologies.at(i)
         name = entry.getNode('name').string()
         datasetName = entry.getNode('dataset').string()
+        useIR = entry.getNode('use_ir').string()
+        useIR = not useIR or useIR.lower() == 'true'
 
-        if name == args.model:
-            metrics = evaluate(args.model, args.images, args.annotations, datasetName, args.classes)
-            if args.output:
-                with open(args.output, 'wt') as f:
+        if not args.model or name == args.model:
+            metrics = evaluate(name, args.images, args.annotations, datasetName,
+                               args.classes, useIR, args.perf)
+            if args.perf:
+                performance[name] = metrics
+            elif args.output:
+                with open(args.output, 'at') as f:
                     writer = csv.writer(f)
-                    writer.writerow(['model'] + args.classes)
-                    writer.writerow([args.model] + [metrics[s] for s in args.classes])
-            sys.exit(0)
+                    writer.writerow([name] + [metrics[s] for s in args.classes])
 
-    print('Unable to find ' + args.model)
-    sys.exit(1)
+    if args.perf:
+        if args.output:
+            with open(args.output, 'at') as f:
+                writer = csv.writer(f)
+                minTime = np.min([v for v in performance.values()])
+                maxTime = np.max([v for v in performance.values()])
+                for name, perf in performance.items():
+                    writer.writerow([name] + [1.0 - (perf - minTime) / (maxTime - minTime)])
+        else:
+            print(performance)
