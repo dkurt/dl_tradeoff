7486a1df1231066780a65511efea22a96ae1e9c8
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2019-09-30 17:06:16 +0300

    Face detection

--- evaluate.py
+++ evaluate.py
@@ -15,7 +15,7 @@ from pycocotools.cocoeval import COCOeval
 from datasets import *
 
 
-def evaluate(model, dataset, annotations, datasetName, classes, useIR, perfTest):
+def evaluate(model, imagesDir, annotations, datasetName, classes, useIR, perfTest):
     net = None
     # We do fake script location to nagivate to extra models from this repo
     fake_loc = os.path.join(os.path.dirname(__file__), 'extra', 'models', 'yolo-v3', 'model.yml')
@@ -47,16 +47,19 @@ def evaluate(model, dataset, annotations, datasetName, classes, useIR, perfTest)
     nmsThreshold = 0.4 if len(net.getUnconnectedOutLayers()) > 1 else 0.0
     confThreshold = 0.01
 
+    with open(annotations) as f:
+        dataset = json.load(f)
+
     detections = []
-    images = os.listdir(dataset)
-    for i, imgName in enumerate(images):
+    for i, datasetEntry in enumerate(dataset['images']):
         if perfTest and i == 10:
             break
-        print("%d/%d" % (i + 1, len(images)))
+        print("%d/%d" % (i + 1, len(dataset['images'])))
 
-        img = cv.imread(os.path.join(dataset, imgName))
+        imgPath = os.path.join(imagesDir, datasetEntry['file_name'])
+        img = cv.imread(imgPath)
         if img is None:
-            print("Unable to read image: " + imgName)
+            print("Unable to read image: " + imgPath)
             continue
 
         # TODO: fix Faster R-CNN with swapped inputs
@@ -98,25 +101,18 @@ def evaluate(model, dataset, annotations, datasetName, classes, useIR, perfTest)
                                                           nmsThreshold=nmsThreshold)
 
         for classId, score, box in zip(classIds, confidences, boxes):
-            classId = int(classId)
-            if datasetName == 'VOC':
-                categoryId = toCOCO(classId, VOC_classes)
-            elif datasetName == 'COCO_80':
-                categoryId = toCOCO(classId, COCO_80_classes)
-            elif datasetName == 'COCO':
-                categoryId = classId
-            else:
-                categoryId = toCOCO(classId, datasetName.split(','))
+            classId = datasetManager.castClassId(int(classId), datasetName)
 
             detections.append({
-              "image_id": int(imgName.rstrip('0')[:imgName.rfind('.')]),
-              "category_id": categoryId,
+              "image_id": datasetEntry['id'],
+              "category_id": classId,
               "bbox": [int(v) for v in box],
               "score": float(score)
             })
 
         #     # Uncomment to render detections
-        #     cv.rectangle(img, tuple(box), (0, 255, 0))
+        #     if classId == 0:
+        #         cv.rectangle(img, tuple(box), (0, 255, 0))
         #
         # # Uncomment to show detections
         # cv.imshow('detections', img)
@@ -130,7 +126,7 @@ def evaluate(model, dataset, annotations, datasetName, classes, useIR, perfTest)
         json.dump(detections, f)
 
     #initialize COCO ground truth api
-    cocoGt=COCO(os.path.join(annotations, 'instances_val2017.json'))
+    cocoGt=COCO(annotations)
 
     #initialize COCO detections api
     cocoDt=cocoGt.loadRes(resFile)
@@ -139,12 +135,8 @@ def evaluate(model, dataset, annotations, datasetName, classes, useIR, perfTest)
 
     metrics = {}
     for className in classes:
-        if not className in COCO_classes:
-            print('Class name not found: ' + className)
-            sys.exit(1)
-
         print('======== %s ========' % className)
-        cocoEval.params.catIds = COCO_classes.index(className)
+        cocoEval.params.catIds = datasetManager.getClassId(className)
         cocoEval.evaluate()
         cocoEval.accumulate()
         cocoEval.summarize()
@@ -167,6 +159,8 @@ if __name__ == '__main__':
                         help='Path to a config file')
     parser.add_argument('-p', dest='perf', action='store_true',
                         help='Run performance test')
+    parser.add_argument('-d', dest='dataset', choices=['COCO', 'FDDB'], required=True,
+                        help='Base evaluation dataset')
     parser.add_argument('--classes', nargs='+', type=str, required=True,
                         help='A list of classes to be evaluated')
     args = parser.parse_args()
@@ -175,6 +169,13 @@ if __name__ == '__main__':
         with open(args.output, 'wt') as f:
             writer = csv.writer(f)
             writer.writerow(['model'] + ['performance (ralative)'] if args.perf else args.classes)
+
+    if args.dataset == 'COCO':
+        datasetManager = COCODataset()
+    elif args.dataset == 'FDDB':
+        datasetManager = FDDBDataset()
+
+    annotations = datasetManager.getAnnotations(args.annotations, args.images)
 
     fs = cv.FileStorage(args.config, cv.FILE_STORAGE_READ)
     topologies = fs.getNode('topologies')
@@ -187,7 +188,7 @@ if __name__ == '__main__':
         useIR = not useIR or useIR.lower() == 'true'
 
         if not args.model or name == args.model:
-            metrics = evaluate(name, args.images, args.annotations, datasetName,
+            metrics = evaluate(name, args.images, annotations, datasetName,
                                args.classes, useIR, args.perf)
             if args.perf:
                 performance[name] = metrics
